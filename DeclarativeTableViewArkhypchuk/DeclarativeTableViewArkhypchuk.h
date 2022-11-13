89cb805fa67fc24c8a739e4a31c35df7efa6ec01
Bohdan Arkhypchuk <arkhypchuk.b@gmail.com>
2022-11-13 16:46:45 +0200

    Initial Commit

---
+++ DeclarativeTableViewArkhypchuk/DeclarativeTableViewArkhypchuk.h
@@ -0,0 +1,18 @@
+//
+//  DeclarativeTableViewArkhypchuk.h
+//  DeclarativeTableViewArkhypchuk
+//
+//  Created by Bohdan on 13.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeclarativeTableViewArkhypchuk.
+FOUNDATION_EXPORT double DeclarativeTableViewArkhypchukVersionNumber;
+
+//! Project version string for DeclarativeTableViewArkhypchuk.
+FOUNDATION_EXPORT const unsigned char DeclarativeTableViewArkhypchukVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeclarativeTableViewArkhypchuk/PublicHeader.h>
+
+
